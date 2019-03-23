61156dbef4a5dd2db3494f4ef1ffa9ba471e78fd
lubegasimon <lubegasimonwalker@gmail.com>
2019-03-23 16:01:44 +0300

    feat: from hooks to class component

--- src/views/components/LoginForm/index.tsx
+++ src/views/components/LoginForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Form, Header, Input } from "semantic-ui-react";
 
 interface Credentials {
@@ -11,61 +11,73 @@ interface Props {
   onSubmit: (input: Credentials) => Promise<void>;
 }
 
-function LoginForm(props: Props): any {
-  const [loginData, createLoginForm] = useState({
-    email: "",
-    password: ""
-  });
+class LoginForm extends React.Component<Props, Credentials> {
+  public props: Props;
+  public state: Credentials;
 
-  const handleInput = event => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+
+  }
+
+  public handleInput = event => {
     const name = event.target.name;
     const value = event.target.value;
 
-    createLoginForm({
-      ...loginData,
+    this.setState({
+      ...this.state,
       [name]: value
     });
   };
 
-  const handleSubmitForm = async () => {
-    await props.onSubmit(loginData);
+  public handleSubmitForm = async () => {
+    await this.props.onSubmit({
+      email: this.state.email,
+      password: this.state.password
+    });
   };
 
-  return (
-    <div>
-      <Header as="h3" textAlign="center">
-        Sign In
-      </Header>
-      <Form onSubmit={handleSubmitForm}>
-        <Form.Field>
-          <Input
-            icon="user"
-            iconPosition="left"
-            type="email"
-            name="email"
-            value={loginData.email}
-            placeholder="Email address"
-            autoFocus={true}
-            onChange={handleInput}
-            required={true}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            icon="lock"
-            iconPosition="left"
-            type="password"
-            name="password"
-            value={loginData.password}
-            placeholder="Password"
-            onChange={handleInput}
-            required={true}
-          />
-        </Form.Field>
-        <Button type="submit">Login</Button>
-      </Form>
-    </div>
-  );
+  public render() {
+    return(
+        <div>
+          <Header as="h3" textAlign="center">
+            Sign In
+          </Header>
+          <Form onSubmit={this.handleSubmitForm}>
+            <Form.Field>
+              <Input
+                icon="user"
+                iconPosition="left"
+                type="email"
+                name="email"
+                value={this.state.email}
+                placeholder="Email address"
+                autoFocus={true}
+                onChange={this.handleInput}
+                required={true}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                icon="lock"
+                iconPosition="left"
+                type="password"
+                name="password"
+                value={this.state.password}
+                placeholder="Password"
+                onChange={this.handleInput}
+                required={true}
+              />
+            </Form.Field>
+            <Button type="submit">Login</Button>
+          </Form>
+        </div>
+      );
+  }
 }
 
 export default LoginForm;
