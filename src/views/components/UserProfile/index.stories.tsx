b58d90b81c0cbf5205ba6474f05bbe66cfa22ee9
aleku399 <aleku399@gmail.com>
2019-03-25 12:29:21 +0300

    fix: suggested review changes

--- src/views/components/UserProfile/index.stories.tsx
+++ src/views/components/UserProfile/index.stories.tsx
@@ -3,6 +3,27 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import Profile from "./index";
 
+const roleOptions = [
+  { text: "Users", key: "users" },
+  { text: "Admin", key: "admin" },
+  { text: "Evaluators", key: "evaluators" },
+  { text: "Supervisors", key: "supervisors" },
+  { text: "Agents", key: "agents" }
+];
+
+const profile = {
+  userName: "aleku399",
+  email: "aleku399@gmail.com",
+  password: "captain-marvel",
+  role: "Supervisors"
+}
+
 storiesOf("components/Profile", module).add("default", () => (
-  <Profile autoFocus={true} onSubmit={action("profile")} />
+  <Profile
+    roleOptions={roleOptions}
+    autoFocus={true}
+    onSubmit={action("profile")}
+    profile={profile}
+  />
 ));
+
